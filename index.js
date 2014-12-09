d97869b916eb52f04e53e0c60b6cd64bb7dfb0e5
Andrew Cafourek <andrew@a022digital.com>
2014-12-09 11:01:58 -0500

    Weather widget support

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var requestapi = require('request');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -16,9 +17,24 @@ app.use(express.static(__dirname + '/public'));
 
 //Get Weather widget data
 	app.get('/widgets/weather', function(request, response) {
-	  response.send('working on it');
-	});	
-
+		requestapi('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202459115&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function (error, apiresponse, body) {
+			if (!error && apiresponse.statusCode == 200) {
+			   var weather = JSON.parse(apiresponse.body);
+			   var forecast = weather.query.results.channel.item;
+			   //Format weather data for Geck-o-meter: https://developer.geckoboard.com/#geck-o-meter
+			   var formatted = {
+				   "item": forecast.condition.temp,
+				   "min": {
+					   "value": forecast.forecast[0].low
+				   },
+				   "max": {
+					   "value": forecast.forecast[0].high
+				   }
+			   };
+			   response.send(formatted);
+			}
+		});
+	});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
