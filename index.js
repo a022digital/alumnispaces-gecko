3c72dbe2f5ddd7aed906c862b66e0071f12fc045
Andrew Cafourek <andrew@a022digital.com>
2014-12-09 10:06:25 -0500

    Get / fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 //Default Homepage
-	app.get('/widgets', function(request, response) {
+	app.get('/', function(request, response) {
 		response.send('You have wondered far and come unto a land of glory.');		
 	});
 	
