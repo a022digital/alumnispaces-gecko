69e40d80e0abcb413f4af15d52ddc9aea8548b78
Andrew Cafourek <andrew@a022digital.com>
2014-12-09 10:04:53 -0500

    Functional

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+//Default Homepage
+	app.get('/widgets', function(request, response) {
+		response.send('You have wondered far and come unto a land of glory.');		
+	});
+	
+//Get list of available widget interfaces
+	app.get('/widgets', function(request, response) {
+	  response.send({available:['weather','date']});
+	});
+
+//Get Weather widget data
+	app.get('/widgets/weather', function(request, response) {
+	  response.send('working on it');
+	});	
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
